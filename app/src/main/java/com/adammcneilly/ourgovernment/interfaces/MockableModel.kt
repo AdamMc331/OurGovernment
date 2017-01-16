8931b5b2a6fb1c5ac53a79e6b46798fdee392a64
AdamMc331 <amcneilly331@gmail.com>
2017-01-15 21:23:55 -0500

    Documented MockableModel.kt

--- app/src/main/java/com/adammcneilly/ourgovernment/interfaces/MockableModel.kt
+++ app/src/main/java/com/adammcneilly/ourgovernment/interfaces/MockableModel.kt
@@ -6,6 +6,13 @@ package com.adammcneilly.ourgovernment.interfaces
  * Created by adam.mcneilly on 12/30/16.
  */
 interface MockableModel {
+    /**
+     * @return a list of strings representing successful JSON from the server.
+     */
     fun getSuccessJson(): List<String>
+
+    /**
+     * @return a list of strings representing failure JSON from the server.
+     */
     fun getFailureJson(): List<String>
 }
